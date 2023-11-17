234e8bbfeaa40b600cac0e875a63b1309cdb2d67
Dan Kamrath <dmkamrath@gmail.com>
2023-11-17 12:19:14 -0600

    added clock publisher that uses unix time epoch instead of app start

---
+++ Scripts/Utilities/AtmaClockPublisher.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using ROS2;
+using AWSIM;
+using autonoma_msgs.msg;
+//using std_msgs.msg;
+using rosgraph_msgs.msg;
+//using builtin_interfaces.msg;
+
+namespace Autonoma
+{
+
+public class AtmaClockPublisher : Publisher<rosgraph_msgs.msg.Clock>
+{
+    public String modifiedRosNamespace = "";
+    public String modifiedTopicName = "/clock";
+    public float modifiedFrequency = 100f;
+    public String modifiedFrameId = "";
+
+    protected override void Start()
+    {
+        this.rosNamespace = modifiedRosNamespace;
+        this.topicName = modifiedTopicName;
+        this.frequency = modifiedFrequency;
+        this.frameId = modifiedFrameId;
+        base.Start();
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public override void fillMsg()
+    {
+       TimeSpan t = (DateTime.UtcNow - new DateTime(1970, 1, 1));
+        msg.Clock_.Sec = (int)t.TotalSeconds;
+        msg.Clock_.Nanosec = (uint)((t.TotalSeconds - (uint)t.TotalSeconds) / 1.0e-9);
+    }
+}
+
+}
